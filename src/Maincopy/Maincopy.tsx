9d021efeab0d3dd280fa9e6246de3236b7dfc99d
Molly Saleh <Molly@kdvi-macbook-pro-01.home>
2024-04-22 13:40:52 +0100

    most filters working with eachother on api

--- src/Maincopy/Maincopy.tsx
+++ src/Maincopy/Maincopy.tsx
@@ -5,16 +5,16 @@ import { Beer } from "../Data/beertypes";
 
 type searching = {
     searchTerm: string;
-    data: Beer[];
+    beers: Beer[];
 }
 
-const Maincopy = ({data}: searching) => {
-  console.log(data)
+const Maincopy = ({beers}: searching) => {
+  console.log(beers)
   return (
     <div className="maincopy">
         <h1>PUNK API</h1>
         <div className="maincopy__beers">
-        {data.map((beer) => <Beercard name={beer.name} image_url={beer.image_url} description={beer.description} abv={beer.abv} ph={beer.ph} first_brewed={beer.first_brewed} key={beer.name} />)}
+        {beers.map((beer) => <Beercard name={beer.name} image_url={beer.image_url} description={beer.description} abv={beer.abv} ph={beer.ph} first_brewed={beer.first_brewed} key={beer.name} />)}
         </div>
     </div>
   )
